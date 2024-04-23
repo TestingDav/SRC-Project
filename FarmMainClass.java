8b02f8ce364ff10f6d5261d1760ba2e00e3b58ee
TemporaryUserGithub <159093375+TemporaryUserGithub@users.noreply.github.com>
2024-04-23 14:28:39 -0700

    Update FarmMainClass.java

--- FarmMainClass.java
+++ FarmMainClass.java
@@ -1,38 +1,47 @@
-package social;
+package social; // could change the importance to a stack
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 public class FarmMainClass {
     private String season;
     private int quantityNeeded;
-    private LinkedBag resource;
+    private LinkedBag<String> resource;//change to dictionary
+    private double importance; //1-100
+    private double seasonWeight; // 1.5 for winter, 1.2 for spring, 1 for summer, 1.4 for fall (for example)
+    //create a bag of prices (a dictionary of prices for each resources) x wouldnt work if we need to find the price of a specfic resource. solution: ...
+    private HashMap<String, Double> prices;
     public FarmMainClass() {
         this.season = "winter";
         this.quantityNeeded = 0;
-        this.resource = new LinkedBag();
+        this.resource = new LinkedBag<String>();
         this.resource = null; // or something else
+        this.seasonWeight = (1.5+1.2+1+1.4)/4;
     }
 
-    public FarmMainClass(String season, int quantityNeeded, LinkedBag resource) {
+    public FarmMainClass(String season, int quantityNeeded, LinkedBag<String> resource) {
         this.season = season;
         this.quantityNeeded = quantityNeeded;
         this.resource = resource;
         this.resource = null; // or something else
+        if(season.equalsIgnoreCase("winter")) {
+            this.seasonWeight = 1.5;
+        } else if(season.equalsIgnoreCase("spring")) {
+            this.seasonWeight = 1.2;
+        } else if(season.equalsIgnoreCase("summer")) {
+            this.seasonWeight = 1;
+        } else if(season.equalsIgnoreCase("fall")) {
+            this.seasonWeight = 1.4;
+        }
+    }
+    
+    public List<String> estimateCost(String resourceRequest, int quantityNeeded, HashMap<String, Double> resource, double seasonWeight) {
+        return null;
     }
 
-    public double estimateCost(int quantityNeeded, LinkedBag resource) {
-        // Placeholder variable for pricing
-        double price = 0.0;
-
-        // Calculate estimated cost based on quantity needed and resource
-        double estimatedCost = price * quantityNeeded;
-
-        return estimatedCost;
+    public boolean need(LinkedBag<String> resource) {
+        return importance >= 65;
     }
 
-    public boolean need(LinkedBag resource) {
-        // Check if a resource is needed
-        // Implement logic soon
-        return false;
-    }
     public static void main(String[] args) {
         LinkedBag<String> bag = new LinkedBag<>();
         Scanner scanner = new Scanner(System.in);
